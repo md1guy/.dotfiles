645fd2c23681afab2c6573660e7b77e37dd2beeb
metyslav <volchkov.oleh@gmail.com>
2019-09-23 16:12:22 +0300

    updated lock script

--- .config/lock/lock.sh
+++ .config/lock/lock.sh
@@ -13,32 +13,32 @@ TY='#484BC2bb' # typing
 xkblayout-state set 0;
 
 i3lock \
---insidevercolor=$C   \
---ringvercolor=$V     \
-\
---insidewrongcolor=$C \
---ringwrongcolor=$W   \
-\
---insidecolor=$C      \
---ringcolor=$D        \
---linecolor=$B        \
---separatorcolor=$D   \
-\
---verifcolor=$T        \
---wrongcolor=$T        \
---timecolor=$T        \
---datecolor=$T        \
---layoutcolor=$T      \
---keyhlcolor=$TY       \
---bshlcolor=$W        \
-\
---screen 1            \
---blur 6              \
---clock               \
---indicator           \
---timestr="%H:%M:%S"  \
---datestr="%A, %d/%m" \
---radius 100 \
+	--insidevercolor=$C   \
+	--ringvercolor=$V     \
+	\
+	--insidewrongcolor=$C \
+	--ringwrongcolor=$W   \
+	\
+	--insidecolor=$C      \
+	--ringcolor=$D        \
+	--linecolor=$B        \
+	--separatorcolor=$D   \
+	\
+	--verifcolor=$T        \
+	--wrongcolor=$T        \
+	--timecolor=$T        \
+	--datecolor=$T        \
+	--layoutcolor=$T      \
+	--keyhlcolor=$TY       \
+	--bshlcolor=$W        \
+	\
+	--screen 1            \
+	--blur 6              \
+	--clock               \
+	--indicator           \
+	--timestr="%H:%M"  \
+	--datestr="%A, %d/%m" \
+	--radius 100 \
 
 # --veriftext="Drinking verification can..."
 # --wrongtext="Nope!"
